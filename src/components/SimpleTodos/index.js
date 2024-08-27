5ea711f6724e6e553b69594c9ab965b3a6102ea5
Pambalajagan02 <jaganpambala@gmail.com>
2024-08-27 08:02:26 +0000

    enhanced todo

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,8 +1,7 @@
-import {Component} from 'react'
-
 import './index.css'
-
-import Todoperform from '../TodoItem/index'
+import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
+import TodoItem from '../TodoItem'
 
 const initialTodosList = [
   {
@@ -38,30 +37,75 @@ const initialTodosList = [
     title: 'Get essentials for Sunday car wash',
   },
 ]
+
 // Write your code here
-
 class SimpleTodos extends Component {
-  state = {todolist: initialTodosList}
+  state = {
+    todoList: initialTodosList,
+    inputData: '',
+  }
 
-  deleteToDo = uniqueid => {
-    const {todolist} = this.state
-    const deleteresults = todolist.filter(each => each.id !== uniqueid)
-    this.setState({todolist: deleteresults})
+  deleteTodo = todoId => {
+    const {todoList} = this.state
+    const filteredUsersData = todoList.filter(each => each.id !== todoId)
+    this.setState({
+      todoList: filteredUsersData,
+    })
+  }
+
+  onChangeInput = event => {
+    this.setState({inputData: event.target.value})
+  }
+
+  onAddNewTask = () => {
+    const {inputData} = this.state
+    const newArr = {id: uuidv4(), title: inputData}
+    this.setState(prev => ({
+      todoList: [...prev.todoList, newArr],
+      inputData: '',
+    }))
+  }
+
+  onSaveExitingTask = (id, taskTitle) => {
+    const {todoList} = this.state
+    const newUpdatedList = todoList.map(each => {
+      if (each.id === id) {
+        return {...each, title: taskTitle}
+      }
+      return each
+    })
+    this.setState({todoList: newUpdatedList})
   }
 
   render() {
-    const {todolist} = this.state
-
+    const {todoList, inputData} = this.state
     return (
-      <div className="back-container">
-        <div className="inner-Container">
-          <h1 className="heading-style">Simple Todos</h1>
-          <ul>
-            {todolist.map(each => (
-              <Todoperform
-                todoitem={each}
-                key={each.id}
-                deleteToDo={this.deleteToDo}
+      <div className="bg-container">
+        <div className="todo-card">
+          <h1 className="heading">Simple Todos</h1>
+          <div className="input-div-container">
+            <input
+              type="text"
+              className="input"
+              placeholder="Add Task"
+              value={inputData}
+              onChange={this.onChangeInput}
+            />
+            <button
+              type="button"
+              onClick={this.onAddNewTask}
+              className="add-task-btn"
+            >
+              Add
+            </button>
+          </div>
+          <ul className="todo">
+            {todoList.map(eachTodo => (
+              <TodoItem
+                key={eachTodo.id}
+                todoList={eachTodo}
+                onEditedTask={this.onSaveExitingTask}
+                deleteTodo={this.deleteTodo}
               />
             ))}
           </ul>
@@ -70,4 +114,5 @@ class SimpleTodos extends Component {
     )
   }
 }
+
 export default SimpleTodos
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,22 +1,77 @@
 // Write your code here
+import {useState} from 'react'
+
 import './index.css'
 
-const Todoperform = props => {
-  const {todoitem, deleteToDo} = props
-  const {title, id} = todoitem
-
-  const onToDoDelete = () => {
-    deleteToDo(id)
+const TodoItem = props => {
+  const {deleteTodo, todoList, onEditedTask} = props
+  const {id, title} = todoList
+  const onDeleteTodo = () => {
+    deleteTodo(id)
   }
+  const [isEdit, setIsEdit] = useState(false)
+  const [isChecked, setCheck] = useState(false)
+  const [taskTitle, setTaskTitle] = useState(title)
+
+  const onClickEdit = () => {
+    setIsEdit(true)
+  }
+  const onSaveEditTask = () => {
+    setIsEdit(false)
+    onEditedTask(id, taskTitle)
+  }
+  const onChangeTitle = event => {
+    setTaskTitle(event.target.value)
+  }
+  const onClickCheckInput = event => {
+    setCheck(event.target.checked)
+  }
+
+  const isCheckedTrue = isChecked ? 'checked-title' : ''
+  console.log(isChecked)
   return (
-    <li className="listContainer">
-      <p>{title}</p>
-      <div>
-        <button type="button" className="buttonstyle" onClick={onToDoDelete}>
-          Delete
-        </button>
-      </div>
+    <li className="card">
+      {isEdit ? (
+        <div className="text-btn-div">
+          <input
+            className="edit-input"
+            type="text"
+            value={taskTitle}
+            onChange={onChangeTitle}
+          />
+          <button
+            type="button"
+            className="save-edit-btn"
+            onClick={onSaveEditTask}
+          >
+            Save
+          </button>
+        </div>
+      ) : (
+        <div className="text-btn-div">
+          <div className="input-label-div">
+            <input
+              className="check-box"
+              checked={isChecked}
+              onChange={onClickCheckInput}
+              type="checkbox"
+              id={id}
+            />
+            <p htmlFor={id} className={`title ${isCheckedTrue}`}>
+              {title}
+            </p>
+          </div>
+          <button type="button" className="save-edit-btn" onClick={onClickEdit}>
+            Edit
+          </button>
+        </div>
+      )}
+
+      <button type="button" onClick={onDeleteTodo} className="delete-btn">
+        Delete
+      </button>
     </li>
   )
 }
-export default Todoperform
+
+export default TodoItem
